fcae245ccc603f6c11f424e775f6d0c747f68beb
Dayvid Santana <110176533+kamatashi@users.noreply.github.com>
2024-12-20 09:39:11 -0300

    feat: added string handling functions

--- Documentos/Planilhas/howMuchTime.py
+++ Documentos/Planilhas/howMuchTime.py
@@ -1,11 +1,32 @@
 import csv
 
+
+
 fileTeste = 'deslocamentos.csv'
 
-def takeRow(file):
+
+def square(firstNum, lastNum):
+    hourTaked = int(lastNum[0]) - int(firstNum[0])
+    minTaked = int(lastNum[1]) - int(firstNum[1])
+    return str(hourTaked) + ':' + str(minTaked)
+
+
+def track(dateTrack):
+    time = dateTrack.split(':')
+    hour = time[0]
+    min = time[1]
+    return hour + '.' + min
+
+
+def takeRow(file, firstCell, lastCell):
     with open(file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            print(row['Saída casa'], row['Chegada DIA'])
+            return track(row[firstCell]) - track(row[lastCell])
+        
 
 
+
+#print(takeRow(fileTeste, 'Saída casa', 'Chegada DIA'))
+print(track('45:32:00'))
+print(square(['12', '34'],['14', '9']))
